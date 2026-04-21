188a74dd63e8634e06f9e400cda107bad947dbf4
AngelDPLL <angeld0606@gmail.com>
2026-04-21 16:53:32 +0200

    fix: Bugs

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from app.extensions import db
+from flask_migrate import upgrade
 
 app = create_app()
 
-if __name__ == '__main__':
+with app.app_context():
+    upgrade()
+
+if __name__ == "__main__":
     app.run(debug=True)
